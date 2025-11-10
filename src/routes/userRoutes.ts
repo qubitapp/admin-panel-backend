f88fed7bfec512643c861ce8829bbc10c6739560
codewithsanjay2002-wq <codewithsanjay2002@gmail.com>
2025-11-10 23:02:47 +0530

    Revert "Added login API with JWT authentication"
    This reverts commit 3b6a532b94d8ab4a292ebae21a4833113b5d059a.

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,6 @@
 import { Router, Request, Response } from "express";
 import { AppDataSource } from "../data.source";
 import { User } from "../entities/User";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-
-dotenv.config();
-
 
 const router = Router();
 const userRepo = AppDataSource.getRepository(User);
@@ -66,42 +60,4 @@ router.delete("/:id", async (req: Request, res: Response): Promise<void> => {
   }
 });
 
-// 🔐 LOGIN API
-router.post("/login", async(req: Request, res: Response)=>{
-  try{
-    const { email, password } = req.body;
-    if(!email || !password){
-      return res.status(400).json({message:"Fields required"});
-    }
-    const user = await userRepo.findOne({ where:{email}});
-    if(!user){
-      return res.status(404).json({ message: "User not found"});
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if(!isPasswordValid){
-      return res.status(401).json({message: "Invalid credential"});
-    }
-
-    const token = jwt.sign(
-      { id: user.id, email: user.email},
-      process.env.JWT_SECRET as string,
-      {expiresIn: "1h"}
-    );
-    return res.status(200).json({
-      message: "Login Successfull",
-      token, 
-      user:{
-        id: user.id,
-        name: user.name,
-        email: user.email,
-      },
-
-    });
-  }
-catch(error){
-  console.error("Login Error:", error);
-    return res.status(500).json({ message: "Server error" });
-}
-});
 export default router;
